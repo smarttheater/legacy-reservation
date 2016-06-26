4e0406a9d303e5330522830927c093cd61dbd8ce
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-06-26 11:42:28 +0900

    TIFF-9 一般フローの一部など。

--- apps/common/models/Reservation/ReservationUtil.ts
+++ apps/common/models/Reservation/ReservationUtil.ts
@@ -1,5 +1,8 @@
 export default class ReservationUtil {
-    public static STATUS_AVAILABLE = 'AVAILABLE';
-    public static STATUS_TEMPORARY = 'TEMPORARY';
-    public static STATUS_RESERVED = 'RESERVED';
+    public static STATUS_AVAILABLE = 'AVAILABLE'; // 空席
+    public static STATUS_TEMPORARY = 'TEMPORARY'; // 仮予約
+    public static STATUS_RESERVED = 'RESERVED'; // 予約確定
+    public static STATUS_WAITING_SETTLEMENT = 'WAITING_SETTLEMENT'; // 決済待ち
+    public static STATUS_WAITING_SETTLEMENT_WINDOW = 'WAITING_SETTLEMENT_WINDOW'; // 窓口清算待ち
+    public static STATUS_KEPT_BY_TIFF = 'KEPT_BY_TIFF'; // 関係者席保留
 }
