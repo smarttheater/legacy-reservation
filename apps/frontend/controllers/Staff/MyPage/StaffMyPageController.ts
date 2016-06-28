9846e339245908ed7f634388cc5ccbad0f901ef1
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-06-28 13:06:14 +0900

    TIFF-9 内部関係者フロー更新。

--- apps/frontend/controllers/Staff/MyPage/StaffMyPageController.ts
+++ apps/frontend/controllers/Staff/MyPage/StaffMyPageController.ts
@@ -1,13 +1,55 @@
 import BaseController from '../../BaseController';
 import StaffUser from '../../../models/User/StaffUser';
 import Util from '../../../../common/Util/Util';
+import ReservationUtil from '../../../../common/models/Reservation/ReservationUtil';
 import Models from '../../../../common/models/Models';
 import mongoose = require('mongoose');
 
 export default class StaffMyPageController extends BaseController {
     public index(): void {
-        this.res.render('staff/mypage/index', {
-            layout: 'layouts/staff/layout'
+        this.useMongoose(() => {
+            Models.Reservation.find(
+                {
+                    staff: this.staffUser.get('_id'),
+                    status: ReservationUtil.STATUS_RESERVED
+                },
+                {},
+                {
+                    sort : {staff: 1, seat_code: 1},
+                    limit: 10
+                }
+            ).populate('staff screen').exec((err, reservationDocuments) => {
+                mongoose.disconnect(() => {
+
+                    if (err) {
+
+                    } else {
+                        // 予約番号ごとに整形
+                        let reservationDocumentsByPaymentNo = {};
+                        let screenSeatCodesByPaymentNo = {};
+                        for (let reservationDocument of reservationDocuments) {
+                            let paymentNo = reservationDocument.get('payment_no');
+                            if (!reservationDocumentsByPaymentNo.hasOwnProperty(paymentNo)) {
+                                reservationDocumentsByPaymentNo[paymentNo] = [];
+
+                                let seats = reservationDocument.get('screen').get('sections')[0].get('seats');
+                                screenSeatCodesByPaymentNo[paymentNo] = [];
+                                for (let seat of seats) {
+                                    screenSeatCodesByPaymentNo[paymentNo].push(seat.get('code'));
+                                }
+                            }
+
+                            reservationDocumentsByPaymentNo[paymentNo].push(reservationDocument);
+                        }
+
+                        this.res.render('staff/mypage/index', {
+                            layout: 'layouts/staff/layout',
+                            reservationDocumentsByPaymentNo: reservationDocumentsByPaymentNo,
+                            screenSeatCodesByPaymentNo: screenSeatCodesByPaymentNo,
+                        });
+                    }
+                });
+            });
         });
     }
 }
