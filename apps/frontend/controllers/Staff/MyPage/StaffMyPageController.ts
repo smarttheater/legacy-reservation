1a67a9115c8d032ff081595e0577d33e14698d3c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-06-30 10:48:36 +0900

    TIFF-9 内部関係者マイページ更新。

--- apps/frontend/controllers/Staff/MyPage/StaffMyPageController.ts
+++ apps/frontend/controllers/Staff/MyPage/StaffMyPageController.ts
@@ -17,23 +17,59 @@ export default class StaffMyPageController extends BaseController {
     public search(): void {
         let limit = 2;
         let page = (this.req.query.page) ? this.req.query.page : 1;
+        let day = (this.req.query.day) ? this.req.query.day : null;
+        let startTime = (this.req.query.start_time) ? this.req.query.start_time : null;
         let film = (this.req.query.film) ? this.req.query.film : null;
+        let words = (this.req.query.words) ? this.req.query.words : null;
 
         // 検索条件を作成
-        let conditions = {
-            staff: this.staffUser.get('_id'),
-            status: ReservationUtil.STATUS_RESERVED
-        };
+        let conditions: Array<Object> = [];
+
+        conditions.push({staff: this.staffUser.get('_id')});
+        conditions.push({status: ReservationUtil.STATUS_RESERVED});
 
         if (film) {
-            conditions['film'] = film;
+            conditions.push({film: film});
         }
+        if (day) {
+            conditions.push({performance_day: day});
+        }
+        if (startTime) {
+            conditions.push({
+                performance_start_time: {
+                    $gte: startTime,
+                }
+            });
+        }
+        if (words) {
+            conditions.push({
+                $or: [
+                    {
+                        film_name: {$regex: `${words}`}
+                    },
+                    {
+                        staff_signature: {$regex: `${words}`}
+                    },
+                    {
+                        watcher_name: {$regex: `${words}`}
+                    }
+                ]
+            });
+        }
+
+
+
+        // 総数検索
         Models.Reservation.count(
-            conditions,
+            {
+                $and: conditions
+            },
             (err, count) => {
 
                 Models.Reservation.find(
-                    conditions,
+                    {
+                        $and: conditions
+                    },
                     {},
                     {
                         sort : {staff: 1, seat_code: 1}
@@ -51,7 +87,7 @@ export default class StaffMyPageController extends BaseController {
 
                         this.res.json({
                             isSuccess: true,
-                            conditions: conditions,
+                            // conditions: conditions,
                             results: reservationDocuments,
                             count: count
                         });
