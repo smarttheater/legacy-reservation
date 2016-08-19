512a4b0daf086a4e20249ee87e7a6f42bcc02d83
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-08-19 18:19:29 +0900

    TIFF-49 ペイデザイン結果通知待ちの準備。

--- apps/frontend/controllers/PayDesign/Reserve/PayDesignReserveController.ts
+++ apps/frontend/controllers/PayDesign/Reserve/PayDesignReserveController.ts
@@ -12,9 +12,10 @@ export default class PayDesignReserveController extends ReserveBaseController {
      * ペイデザイン入金通知
      */
     public notify(): void {
+        this.logger.info('PayDesignReserveController notify start.', this.req);
         this.logger.info('PayDesignReserveController notify start.', this.req.method);
         this.logger.info('PayDesignReserveController notify start.', this.req.originalUrl);
-        this.logger.info('PayDesignReserveController notify start.', this.req.query);
+        this.logger.info('PayDesignReserveController notify start.', this.req.query.text);
         this.logger.info('PayDesignReserveController notify start.', this.req.body);
         let gmoNotificationModel = PayDesignNotificationModel.parse(this.req.body);
         this.logger.info('gmoNotificationModel is', gmoNotificationModel);
@@ -26,6 +27,7 @@ export default class PayDesignReserveController extends ReserveBaseController {
      * ペイデザイン取消通知
      */
     public cancel(): void {
+        this.logger.info('PayDesignReserveController notify start.', this.req);
         this.logger.info('PayDesignReserveController cancel start.', this.req.method);
         this.logger.info('PayDesignReserveController cancel start.', this.req.originalUrl);
         this.logger.info('PayDesignReserveController cancel start.', this.req.query);
