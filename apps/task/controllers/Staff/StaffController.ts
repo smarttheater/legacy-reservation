74ceaa96dc548c3138c53d615cc63e2203fc06eb
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-09-01 22:50:57 +0900

    TIFF-121 仮抑え大量インサート部分を調整。

--- apps/task/controllers/Staff/StaffController.ts
+++ apps/task/controllers/Staff/StaffController.ts
@@ -1,8 +1,8 @@
 import BaseController from '../BaseController';
-import Constants from '../../../common/Util/Constants';
 import Util from '../../../common/Util/Util';
 import Models from '../../../common/models/Models';
 import conf = require('config');
+import mongodb = require('mongodb');
 import mongoose = require('mongoose');
 import fs = require('fs-extra');
 
@@ -50,6 +50,7 @@ export default class StaffController extends BaseController {
             let i = 0;
             let next = () => {
                 if (i < screenIds.length) {
+                    this.logger.debug('createStaffReservationsByScreenId processing...', screenIds[i].toString());
                     this.createStaffReservationsByScreenId(screenIds[i].toString(), (err) => {
                         i++;
                         next();
@@ -66,8 +67,6 @@ export default class StaffController extends BaseController {
     }
 
     private createStaffReservationsByScreenId(screenId: string, cb: (err: Error) => void): void {
-        let promises = [];
-
         fs.readFile(`${process.cwd()}/data/${process.env.NODE_ENV}/staffReservations_${screenId}.json`, 'utf8', (err, data) => {
             if (err) {
                 this.logger.info('no reservations.');
@@ -81,85 +80,89 @@ export default class StaffController extends BaseController {
                     staffsByName[staff.get('name')] = staff;
                 }
 
-                // スクリーンのパフォーマンスをすべて取得
-                Models.Performance.find(
-                    {
-                        screen: screenId
-                    },
-                    'day start_time end_time film screen theater' // 必要な項目だけ指定すること
-                )
-                .populate('film', 'name ticket_type_group image is_mx4d') // 必要な項目だけ指定すること
-                .populate('screen', 'name') // 必要な項目だけ指定すること
-                .populate('theater', 'name') // 必要な項目だけ指定すること
-                .exec((err, performances) => {
-                    for (let performance of performances) {
-                        let reservations = JSON.parse(data);
-                        reservations = reservations.map((reservation) => {
-                            // 以下項目を共通でreservationに追加する
-                            return Object.assign(reservation, {
-                                "performance": performance.get('_id'),
-                                "status": "RESERVED",
-                                "staff": staffsByName[reservation.staff_name].get('_id'),
-                                "staff_user_id": staffsByName[reservation.staff_name].get('user_id'),
-                                "staff_email": staffsByName[reservation.staff_name].get('email'),
-                                "staff_signature": "system",
-                                "entered": false,
-                                "updated_user": "system",
-                                "purchased_at": "2016-09-01T06:38:28Z",
-                                "watcher_name_updated_at": null,
-                                "watcher_name": "",
-                                "film_is_mx4d": performance.get('film').get('is_mx4d'),
-                                "film_image": performance.get('film').get('image'),
-                                "film_name_en": performance.get('film').get('name.en'),
-                                "film_name_ja": performance.get('film').get('name.ja'),
-                                "film": performance.get('film').get('_id'),
-                                "screen_name_en": performance.get('screen').get('name.en'),
-                                "screen_name_ja": performance.get('screen').get('name.ja'),
-                                "screen": performance.get('screen').get('_id'),
-                                "theater_name_en": performance.get('theater').get('name.en'),
-                                "theater_name_ja": performance.get('theater').get('name.ja'),
-                                "theater": performance.get('theater').get('_id'),
-                                "performance_end_time": performance.get('end_time'),
-                                "performance_start_time": performance.get('start_time'),
-                                "performance_day": performance.get('day'),
-                                "purchaser_group": "04",
-                                "payment_no": "1111118119", // TODO どうしよう？
-                                "charge": 0,
-                                "total_charge": 0,
-                                "ticket_type_charge": 0,
-                                "ticket_type_name_en": "Free",
-                                "ticket_type_name_ja": "無料",
-                                "ticket_type_code": "00",
-                                "seat_grade_additional_charge": 0,
-                                "seat_grade_name_en": "Normal Seat",
-                                "seat_grade_name_ja": "ノーマルシート"
-                            });
-                        });
+                Models.Sequence.findOneAndUpdate(
+                    {target: 'payment_no'},
+                    {$inc: {no: 1}},
+                    {new: true},
+                    (err, sequence) => {
+                        if (err) {
+                            cb(err);
+                        } else {
+                            let no: number = sequence.get('no');
+                            let paymentNo = `${no}${Util.getCheckDigit(no)}`;
+                            let reservations = [];
 
-                        promises.push(new Promise((resolve, reject) => {
-                            this.logger.debug('creating staff reservations...');
-                            Models.Reservation.create(
-                                reservations,
-                                (err) => {
-                                    this.logger.info('staff reservations created.', err);
-                                    if (err) {
-                                        reject(err);
-                                    } else {
-                                        resolve();
-                                    }
+                            this.logger.debug('paymentNo is', paymentNo);
+                            // スクリーンのパフォーマンスをすべて取得
+                            Models.Performance.find(
+                                {
+                                    screen: screenId
+                                },
+                                'day start_time end_time film screen theater'
+                            )
+                            .populate('film', 'name ticket_type_group image is_mx4d')
+                            .populate('screen', 'name')
+                            .populate('theater', 'name')
+                            .exec((err, performances) => {
+                                for (let performance of performances) {
+                                    let reservationsByPerformance = JSON.parse(data);
+                                    reservationsByPerformance = reservationsByPerformance.map((reservation) => {
+                                        // 以下項目を共通でreservationに追加する
+                                        return Object.assign(reservation, {
+                                            "performance": performance.get('_id'),
+                                            "status": "RESERVED",
+                                            "staff": staffsByName[reservation.staff_name].get('_id'),
+                                            "staff_user_id": staffsByName[reservation.staff_name].get('user_id'),
+                                            "staff_email": staffsByName[reservation.staff_name].get('email'),
+                                            "staff_signature": "system",
+                                            "entered": false,
+                                            "updated_user": "system",
+                                            "purchased_at": "2016-09-01T06:38:28Z",
+                                            "watcher_name_updated_at": null,
+                                            "watcher_name": "",
+                                            "film_is_mx4d": performance.get('film').get('is_mx4d'),
+                                            "film_image": performance.get('film').get('image'),
+                                            "film_name_en": performance.get('film').get('name.en'),
+                                            "film_name_ja": performance.get('film').get('name.ja'),
+                                            "film": performance.get('film').get('_id'),
+                                            "screen_name_en": performance.get('screen').get('name.en'),
+                                            "screen_name_ja": performance.get('screen').get('name.ja'),
+                                            "screen": performance.get('screen').get('_id'),
+                                            "theater_name_en": performance.get('theater').get('name.en'),
+                                            "theater_name_ja": performance.get('theater').get('name.ja'),
+                                            "theater": performance.get('theater').get('_id'),
+                                            "performance_end_time": performance.get('end_time'),
+                                            "performance_start_time": performance.get('start_time'),
+                                            "performance_day": performance.get('day'),
+                                            "purchaser_group": "04",
+                                            "payment_no": paymentNo,
+                                            "charge": 0,
+                                            "total_charge": 0,
+                                            "ticket_type_charge": 0,
+                                            "ticket_type_name_en": "Free",
+                                            "ticket_type_name_ja": "無料",
+                                            "ticket_type_code": "00",
+                                            "seat_grade_additional_charge": 0,
+                                            "seat_grade_name_en": "Normal Seat",
+                                            "seat_grade_name_ja": "ノーマルシート"
+                                        });
+                                    });
+
+                                    reservations = reservations.concat(reservationsByPerformance);
                                 }
-                            );
-                        }));
-                    }
 
-                    Promise.all(promises).then(() => {
-                        this.logger.info('promised.');
-                        cb(null);
-                    }, (err) => {
-                        this.logger.info('promised.', err);
-                        cb(err);
-                    });
-                });
+                                let MongoClient = mongodb.MongoClient;
+                                MongoClient.connect(conf.get<string>('mongolab_uri'), (err, db) => {
+                                    this.logger.debug('creating staff reservations...length:', reservations.length);
+                                    db.collection('reservations').insertMany(reservations, (err, result) => {
+                                        db.close();
+                                        cb(err);
+                                    });
+                                });
+                            });
+                        }
+                    }
+                );
             });
         });
     }
