7f03188277ee2dda7b7bcfd3ea4e7bf7d9f0b635
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-09-16 15:27:48 +0900

    TIFF-158 インターン説明会用のデータを作成。

---
+++ bin/intialize_db_pre.bat
@@ -0,0 +1,22 @@
+cd ../
+set NODE_ENV=pre
+node bin/command schema dropIndexes
+node bin/command schema createIndexes
+
+node bin/command film createTicketTypeGroupsFromJson
+node bin/command film createFromJson
+node bin/command theater createFromJson
+node bin/command theater createScreensFromJson
+node bin/command performance createFromJson
+node bin/command reservation reset
+
+node bin/command member createFromJson
+node bin/command sponsor createFromJson
+node bin/command staff createFromJson
+node bin/command tel createFromJson
+node bin/command window createFromJson
+
+node bin/command member createReservationsFromJson
+node bin/command staff createReservationsFromJson
+
+pause
