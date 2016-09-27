009d83092e8b1f619a0e9bbf16471798780a783b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-09-27 22:03:45 +0900

    TIFF-33 静的ページ調整。座席表調整。

--- apps/frontend/controllers/Other/OtherController.js
+++ apps/frontend/controllers/Other/OtherController.js
@@ -2,13 +2,13 @@
 const BaseController_1 = require('../BaseController');
 class OtherController extends BaseController_1.default {
     policy() {
-        this.res.render('other/policy');
+        this.res.render(`other/policy_${this.req.getLocale()}`);
     }
     privacy() {
-        this.res.render('other/privacy');
+        this.res.render(`other/privacy_${this.req.getLocale()}`);
     }
     commercialTransactions() {
-        this.res.render('other/commercialTransactions');
+        this.res.render(`other/commercialTransactions_${this.req.getLocale()}`);
     }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
--- apps/frontend/controllers/Other/OtherController.ts
+++ apps/frontend/controllers/Other/OtherController.ts
@@ -2,14 +2,14 @@ import BaseController from '../BaseController';
 
 export default class OtherController extends BaseController {
     public policy(): void {
-        this.res.render('other/policy');
+        this.res.render(`other/policy_${this.req.getLocale()}`);
     }
 
     public privacy(): void {
-        this.res.render('other/privacy');
+        this.res.render(`other/privacy_${this.req.getLocale()}`);
     }
 
     public commercialTransactions(): void {
-        this.res.render('other/commercialTransactions');
+        this.res.render(`other/commercialTransactions_${this.req.getLocale()}`);
     }
 }
