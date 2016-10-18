72553050cd5180255ff5895b3f5dfcdd718b4a08
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-10-18 14:03:16 +0900

    テスト環境ベーシック認証解除。

--- apps/frontend/middlewares/basicAuth.ts
+++ apps/frontend/middlewares/basicAuth.ts
@@ -3,7 +3,7 @@ import basicAuth = require('basic-auth');
 
 export default (req: express.Request, res: express.Response, next: express.NextFunction) => {
     if (process.env.NODE_ENV === 'prod') return next();
-    if (process.env.NODE_ENV === 'prod02') return next();
+    if (process.env.NODE_ENV === 'test') return next();
     if (req.originalUrl === '/GMO/reserve/notify') return next(); // GMO結果通知に対してはオープンにする
 
     let user = basicAuth(req);
