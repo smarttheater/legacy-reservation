85143bbf79a6a977f7a3597917fc4526e4e1057e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-03-27 15:39:34 +0900

    CHEVRE-13 fix tslint for router[window]

--- app/routes/window.js
+++ app/routes/window.js
@@ -22,90 +22,80 @@ const WindowMyPageController_1 = require("../controllers/Window/MyPage/WindowMyP
 const WindowReserveController_1 = require("../controllers/Window/Reserve/WindowReserveController");
 const WindowUser_1 = require("../models/User/WindowUser");
 exports.default = (app) => {
-    const authenticationMiddleware = (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    const authentication = (req, res, next) => __awaiter(this, void 0, void 0, function* () {
         if (req.windowUser === undefined) {
             next(new Error(req.__('Message.UnexpectedError')));
             return;
         }
-        if (!req.windowUser.isAuthenticated()) {
-            // 自動ログインチェック
-            const checkRemember = (cb) => __awaiter(this, void 0, void 0, function* () {
-                if (req.cookies.remember_window !== undefined) {
-                    try {
-                        const authentication = yield chevre_domain_1.Models.Authentication.findOne({
-                            token: req.cookies.remember_window,
-                            window: { $ne: null }
-                        }).exec();
-                        if (authentication === null) {
-                            res.clearCookie('remember_window');
-                            cb(null);
-                            return;
-                        }
-                        // トークン再生成
-                        const token = Util.createToken();
-                        yield authentication.update({
-                            token: token
-                        }).exec();
-                        // tslint:disable-next-line:no-cookies
-                        res.cookie('remember_window', token, { path: '/', httpOnly: true, maxAge: 604800000 });
-                        const window = yield chevre_domain_1.Models.Window.findOne({ _id: authentication.get('window') }).exec();
-                        cb(window);
-                    }
-                    catch (error) {
-                        cb(null);
-                        return;
-                    }
-                }
-                else {
-                    cb(null);
-                }
-            });
-            yield checkRemember((user) => {
-                if (user !== null && req.session !== undefined) {
-                    // ログインしてリダイレクト
-                    req.session[WindowUser_1.default.AUTH_SESSION_NAME] = user.toObject();
-                    // if exist parameter cb, redirect to cb.
-                    res.redirect(req.originalUrl);
-                }
-                else {
-                    if (req.xhr) {
-                        res.json({
-                            message: 'login required.'
-                        });
-                    }
-                    else {
-                        res.redirect(`/window/login?cb=${req.originalUrl}`);
-                    }
-                }
-            });
-        }
-        else {
+        // 既ログインの場合
+        if (req.windowUser.isAuthenticated()) {
             // 言語設定
             if (req.windowUser.get('locale') !== undefined && req.windowUser.get('locale') !== null) {
                 req.setLocale(req.windowUser.get('locale'));
             }
             next();
+            return;
+        }
+        // 自動ログインチェック
+        const checkRemember = () => __awaiter(this, void 0, void 0, function* () {
+            if (req.cookies.remember_window === undefined) {
+                return null;
+            }
+            try {
+                const authenticationDoc = yield chevre_domain_1.Models.Authentication.findOne({
+                    token: req.cookies.remember_window,
+                    window: { $ne: null }
+                }).exec();
+                if (authenticationDoc === null) {
+                    res.clearCookie('remember_window');
+                    return null;
+                }
+                // トークン再生成
+                const token = Util.createToken();
+                yield authenticationDoc.update({ token: token }).exec();
+                // tslint:disable-next-line:no-cookies
+                res.cookie('remember_window', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+                return yield chevre_domain_1.Models.Window.findOne({ _id: authenticationDoc.get('window') }).exec();
+            }
+            catch (error) {
+                return null;
+            }
+        });
+        const user = yield checkRemember();
+        if (user !== null && req.session !== undefined) {
+            // ログインしてリダイレクト
+            req.session[WindowUser_1.default.AUTH_SESSION_NAME] = user.toObject();
+            res.redirect(req.originalUrl);
+        }
+        else {
+            if (req.xhr) {
+                res.json({ message: 'login required.' });
+            }
+            else {
+                res.redirect(`/window/login?cb=${req.originalUrl}`);
+            }
         }
     });
     // tslint:disable-next-line:variable-name
-    const baseMiddleware = (req, _res, next) => {
+    const base = (req, _res, next) => {
         // 基本的に日本語
         req.setLocale('ja');
         req.windowUser = WindowUser_1.default.parse(req.session);
         next();
     };
     // 当日窓口フロー
-    app.all('/window/login', 'window.mypage.login', baseMiddleware, (req, res, next) => { (new WindowAuthController_1.default(req, res, next)).login(); });
-    app.all('/window/logout', 'window.logout', baseMiddleware, (req, res, next) => { (new WindowAuthController_1.default(req, res, next)).logout(); });
-    app.all('/window/mypage', 'window.mypage', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowMyPageController_1.default(req, res, next)).index(); });
-    app.get('/window/mypage/search', 'window.mypage.search', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowMyPageController_1.default(req, res, next)).search(); });
-    app.get('/window/reserve/start', 'window.reserve.start', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).start(); });
-    app.all('/window/reserve/:token/terms', 'window.reserve.terms', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).terms(); });
-    app.all('/window/reserve/:token/performances', 'window.reserve.performances', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).performances(); });
-    app.all('/window/reserve/:token/seats', 'window.reserve.seats', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).seats(); });
-    app.all('/window/reserve/:token/tickets', 'window.reserve.tickets', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).tickets(); });
-    app.all('/window/reserve/:token/profile', 'window.reserve.profile', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).profile(); });
-    app.all('/window/reserve/:token/confirm', 'window.reserve.confirm', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).confirm(); });
-    app.get('/window/reserve/:paymentNo/complete', 'window.reserve.complete', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).complete(); });
-    app.post('/window/cancel/execute', 'window.cancel.execute', baseMiddleware, authenticationMiddleware, (req, res, next) => { (new WindowCancelController_1.default(req, res, next)).execute(); });
+    // tslint:disable:max-line-length
+    app.all('/window/login', 'window.mypage.login', base, (req, res, next) => { (new WindowAuthController_1.default(req, res, next)).login(); });
+    app.all('/window/logout', 'window.logout', base, (req, res, next) => { (new WindowAuthController_1.default(req, res, next)).logout(); });
+    app.all('/window/mypage', 'window.mypage', base, authentication, (req, res, next) => { (new WindowMyPageController_1.default(req, res, next)).index(); });
+    app.get('/window/mypage/search', 'window.mypage.search', base, authentication, (req, res, next) => { (new WindowMyPageController_1.default(req, res, next)).search(); });
+    app.get('/window/reserve/start', 'window.reserve.start', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).start(); });
+    app.all('/window/reserve/:token/terms', 'window.reserve.terms', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).terms(); });
+    app.all('/window/reserve/:token/performances', 'window.reserve.performances', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).performances(); });
+    app.all('/window/reserve/:token/seats', 'window.reserve.seats', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).seats(); });
+    app.all('/window/reserve/:token/tickets', 'window.reserve.tickets', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).tickets(); });
+    app.all('/window/reserve/:token/profile', 'window.reserve.profile', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).profile(); });
+    app.all('/window/reserve/:token/confirm', 'window.reserve.confirm', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).confirm(); });
+    app.get('/window/reserve/:paymentNo/complete', 'window.reserve.complete', base, authentication, (req, res, next) => { (new WindowReserveController_1.default(req, res, next)).complete(); });
+    app.post('/window/cancel/execute', 'window.cancel.execute', base, authentication, (req, res, next) => { (new WindowCancelController_1.default(req, res, next)).execute(); });
 };
--- app/routes/window.ts
+++ app/routes/window.ts
@@ -6,7 +6,6 @@
  */
 import { Models } from '@motionpicture/chevre-domain';
 import { NextFunction, Request, Response } from 'express';
-import { Document } from 'mongoose';
 import * as Util from '../../common/Util/Util';
 import WindowAuthController from '../controllers/Window/Auth/WindowAuthController';
 import WindowCancelController from '../controllers/Window/Cancel/WindowCancelController';
@@ -15,80 +14,70 @@ import WindowReserveController from '../controllers/Window/Reserve/WindowReserve
 import WindowUser from '../models/User/WindowUser';
 
 export default (app: any) => {
-    const authenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const authentication = async (req: Request, res: Response, next: NextFunction) => {
         if (req.windowUser === undefined) {
             next(new Error(req.__('Message.UnexpectedError')));
             return;
         }
 
-        if (!req.windowUser.isAuthenticated()) {
-            // 自動ログインチェック
-            const checkRemember = async (cb: (user: Document | null) => void) => {
-                if (req.cookies.remember_window !== undefined) {
-                    try {
-                        const authentication = await Models.Authentication.findOne(
-                            {
-                                token: req.cookies.remember_window,
-                                window: { $ne: null }
-                            }
-                        ).exec();
-
-                        if (authentication === null) {
-                            res.clearCookie('remember_window');
-                            cb(null);
-                            return;
-                        }
-
-                        // トークン再生成
-                        const token = Util.createToken();
-                        await authentication.update(
-                            {
-                                token: token
-                            }
-                        ).exec();
-
-                        // tslint:disable-next-line:no-cookies
-                        res.cookie('remember_window', token, { path: '/', httpOnly: true, maxAge: 604800000 });
-                        const window = await Models.Window.findOne({ _id: authentication.get('window') }).exec();
-                        cb(window);
-                    } catch (error) {
-                        cb(null);
-                        return;
-                    }
-                } else {
-                    cb(null);
-                }
-            };
-
-            await checkRemember((user) => {
-                if (user !== null && req.session !== undefined) {
-                    // ログインしてリダイレクト
-                    req.session[WindowUser.AUTH_SESSION_NAME] = user.toObject();
-
-                    // if exist parameter cb, redirect to cb.
-                    res.redirect(req.originalUrl);
-                } else {
-                    if (req.xhr) {
-                        res.json({
-                            message: 'login required.'
-                        });
-                    } else {
-                        res.redirect(`/window/login?cb=${req.originalUrl}`);
-                    }
-                }
-            });
-        } else {
+        // 既ログインの場合
+        if (req.windowUser.isAuthenticated()) {
             // 言語設定
             if (req.windowUser.get('locale') !== undefined && req.windowUser.get('locale') !== null) {
                 req.setLocale(req.windowUser.get('locale'));
             }
 
             next();
+            return;
+        }
+
+        // 自動ログインチェック
+        const checkRemember = async () => {
+            if (req.cookies.remember_window === undefined) {
+                return null;
+            }
+
+            try {
+                const authenticationDoc = await Models.Authentication.findOne(
+                    {
+                        token: req.cookies.remember_window,
+                        window: { $ne: null }
+                    }
+                ).exec();
+
+                if (authenticationDoc === null) {
+                    res.clearCookie('remember_window');
+                    return null;
+                }
+
+                // トークン再生成
+                const token = Util.createToken();
+                await authenticationDoc.update({ token: token }).exec();
+
+                // tslint:disable-next-line:no-cookies
+                res.cookie('remember_window', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+                return await Models.Window.findOne({ _id: authenticationDoc.get('window') }).exec();
+            } catch (error) {
+                return null;
+            }
+        };
+
+        const user = await checkRemember();
+        if (user !== null && req.session !== undefined) {
+            // ログインしてリダイレクト
+            req.session[WindowUser.AUTH_SESSION_NAME] = user.toObject();
+            res.redirect(req.originalUrl);
+        } else {
+            if (req.xhr) {
+                res.json({ message: 'login required.' });
+            } else {
+                res.redirect(`/window/login?cb=${req.originalUrl}`);
+            }
         }
     };
 
     // tslint:disable-next-line:variable-name
-    const baseMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+    const base = (req: Request, _res: Response, next: NextFunction) => {
         // 基本的に日本語
         req.setLocale('ja');
         req.windowUser = WindowUser.parse(req.session);
@@ -96,17 +85,18 @@ export default (app: any) => {
     };
 
     // 当日窓口フロー
-    app.all('/window/login', 'window.mypage.login', baseMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowAuthController(req, res, next)).login(); });
-    app.all('/window/logout', 'window.logout', baseMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowAuthController(req, res, next)).logout(); });
-    app.all('/window/mypage', 'window.mypage', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowMyPageController(req, res, next)).index(); });
-    app.get('/window/mypage/search', 'window.mypage.search', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowMyPageController(req, res, next)).search(); });
-    app.get('/window/reserve/start', 'window.reserve.start', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).start(); });
-    app.all('/window/reserve/:token/terms', 'window.reserve.terms', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).terms(); });
-    app.all('/window/reserve/:token/performances', 'window.reserve.performances', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).performances(); });
-    app.all('/window/reserve/:token/seats', 'window.reserve.seats', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).seats(); });
-    app.all('/window/reserve/:token/tickets', 'window.reserve.tickets', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).tickets(); });
-    app.all('/window/reserve/:token/profile', 'window.reserve.profile', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).profile(); });
-    app.all('/window/reserve/:token/confirm', 'window.reserve.confirm', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).confirm(); });
-    app.get('/window/reserve/:paymentNo/complete', 'window.reserve.complete', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).complete(); });
-    app.post('/window/cancel/execute', 'window.cancel.execute', baseMiddleware, authenticationMiddleware, (req: Request, res: Response, next: NextFunction) => { (new WindowCancelController(req, res, next)).execute(); });
+    // tslint:disable:max-line-length
+    app.all('/window/login', 'window.mypage.login', base, (req: Request, res: Response, next: NextFunction) => { (new WindowAuthController(req, res, next)).login(); });
+    app.all('/window/logout', 'window.logout', base, (req: Request, res: Response, next: NextFunction) => { (new WindowAuthController(req, res, next)).logout(); });
+    app.all('/window/mypage', 'window.mypage', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowMyPageController(req, res, next)).index(); });
+    app.get('/window/mypage/search', 'window.mypage.search', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowMyPageController(req, res, next)).search(); });
+    app.get('/window/reserve/start', 'window.reserve.start', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).start(); });
+    app.all('/window/reserve/:token/terms', 'window.reserve.terms', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).terms(); });
+    app.all('/window/reserve/:token/performances', 'window.reserve.performances', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).performances(); });
+    app.all('/window/reserve/:token/seats', 'window.reserve.seats', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).seats(); });
+    app.all('/window/reserve/:token/tickets', 'window.reserve.tickets', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).tickets(); });
+    app.all('/window/reserve/:token/profile', 'window.reserve.profile', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).profile(); });
+    app.all('/window/reserve/:token/confirm', 'window.reserve.confirm', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).confirm(); });
+    app.get('/window/reserve/:paymentNo/complete', 'window.reserve.complete', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowReserveController(req, res, next)).complete(); });
+    app.post('/window/cancel/execute', 'window.cancel.execute', base, authentication, (req: Request, res: Response, next: NextFunction) => { (new WindowCancelController(req, res, next)).execute(); });
 };
