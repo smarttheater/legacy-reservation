5f5c27987c3bcdd0060764776e8bbfcb69705dae
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-02-11 23:10:16 +0000

    ### Changed - 注文確定後の余分確保分予約へのextraプロパティ連携を削除

--- src/app/controllers/customer/reserve.ts
+++ src/app/controllers/customer/reserve.ts
@@ -520,28 +520,11 @@ function createPotentialActions(paymentNo: string, reservationModel: ReserveSess
                         // プロジェクト固有の値を連携
                         return {
                             id: r.id,
-                            additionalTicketText: r.additionalTicketText,
+                            // additionalTicketText: r.additionalTicketText,
                             underName: r.underName,
                             additionalProperty: r.additionalProperty
                         };
-                    }),
-                    // 余分確保分の予約にもextraプロパティを連携
-                    ...chevreReservations.filter((r) => {
-                        // 注文アイテムに存在しない予約(余分確保分)にフィルタリング
-                        const orderItem = eventReservations.find(
-                            (eventReservation) => eventReservation.id === r.id
-                        );
-
-                        return orderItem === undefined;
                     })
-                        .map((r) => {
-                            return {
-                                id: r.id,
-                                additionalProperty: [
-                                    { name: 'extra', value: '1' }
-                                ]
-                            };
-                        })
                 ]
             }
         }
@@ -645,8 +628,7 @@ function temporaryReservation2confirmed(params: {
         additionalProperty: [
             ...(Array.isArray(params.reservation.additionalProperty)) ? params.reservation.additionalProperty : [],
             { name: 'paymentSeatIndex', value: params.paymentSeatIndex }
-        ],
-        additionalTicketText: params.reservation.additionalTicketText
+        ]
     };
 }
 
